a624b3e5bfc273803d29c6011ab24279ca3c3bde
mathandcodingacademy <mathandcodingacademy@gmail.com>
2026-02-07 23:12:57 -0400

    Create EX02_if_else_comments_only.py

---
+++ week2/EX02_if_else_comments_only.py
@@ -0,0 +1,16 @@
+# Filename: EX02_if_else_comments_only.py
+# EX02 - if / else (COMMENTS ONLY)
+#
+# Goal:
+#   Write a program that asks:
+#     "Is it Sunday?" (yes/no)
+#
+# If yes:
+#   print("Math and Coding Day!")
+# Else:
+#   print("Can't wait for Sunday...")
+#
+# Rules:
+#   - Use input() once
+#   - Use .strip().lower()
+#   - Use one if and one else
